aa22514368c5a6204700eb2831c7d97594337011
pavi <test@123gmail.com>
2026-03-20 20:02:58 +0530

    add url in api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 // api.js
 
-const BASE_URL = process.env.REACT_APP_API_URL
+const BASE_URL = process.env.REACT_APP_API_URL  || "https://ai-resume-analyser-l5qu.onrender.com";
 
 export const registerUser = async (data) => {
 
